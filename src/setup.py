20f7f69f0420e67612df1a687ce57b9ce2d30112
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2021-03-30 00:24:00 +0200

    add full kernel method

--- src/setup.py
+++ src/setup.py
@@ -4,3 +4,7 @@ from torch.utils import cpp_extension
 setup(name='dcls_cpp',
       ext_modules=[cpp_extension.CppExtension('dcls_cpp', ['src/dcls.cpp','src/cuda/dcls_cuda_kernel.cu'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
+setup(name='dcls_full_cpp',
+      ext_modules=[cpp_extension.CppExtension('dcls_full_cpp', ['src/dcls_full.cpp','src/cuda/dcls_full_cuda_kernel.cu'])],
+      cmdclass={'build_ext': cpp_extension.BuildExtension})
+
