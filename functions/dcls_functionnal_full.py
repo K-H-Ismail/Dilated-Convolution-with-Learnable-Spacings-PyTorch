2a5c00cfdacd1d79d3e73bb737fecf5a597333a8
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2021-05-04 15:49:10 +0200

    add stable dcls1d

--- functions/dcls_functionnal_full.py
+++ functions/dcls_functionnal_full.py
@@ -1,13 +1,48 @@
 import torch
 import numpy as np
+from torch.autograd.function import once_differentiable
 
 import sys
-egg_path='/home/ismail/DeepLabV3Plus-Pytorch/ddd/dist/dcls_full_cpp-0.0.0-py3.8-linux-x86_64.egg'
+egg_path='/home/ismail/Dilated-Convolution-with-Learnable-Spacings-PyTorch/dist/dcls_full_cpp-0.0.0-py3.8-linux-x86_64.egg'
+sys.path.append(egg_path)
+egg_path='/home/ismail/Dilated-Convolution-with-Learnable-Spacings-PyTorch/dist/dcls_1d_cpp-0.0.0-py3.8-linux-x86_64.egg'
 sys.path.append(egg_path)
 
-import dcls_full_cpp
+import dcls_full_cpp, dcls_1d_cpp
+
+class SurrogateDilation1d(torch.autograd.Function):
+    
+    @staticmethod 
+    def forward(ctx, weight, P, dilation):
+        
+        ctx.dilation = dilation        
+        
+        ctx.save_for_backward(weight, P)
+        
+        output = dcls_1d_cpp.forward(weight,
+                                       P,
+                                       ctx.dilation
+                                      )
+
+        return output
 
 
+    @staticmethod  
+    def backward(ctx, grad_output):
+                  
+        weight, P = ctx.saved_tensors
+
+        outputs = dcls_1d_cpp.backward(weight, 
+                                         P, 
+                                         grad_output.contiguous(),
+                                         ctx.dilation
+                                        )
+        
+        grad_weight, grad_P = outputs
+
+
+        return grad_weight, grad_P, None
+    
 class SurrogateDilationFull(torch.autograd.Function):
     
     @staticmethod 
@@ -26,7 +61,7 @@ class SurrogateDilationFull(torch.autograd.Function):
         return output
 
 
-    @staticmethod
+    @staticmethod   
     def backward(ctx, grad_output):
                   
         weight, P1, P2 = ctx.saved_tensors
