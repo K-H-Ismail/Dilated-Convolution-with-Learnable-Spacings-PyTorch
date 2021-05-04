6d2d39d10718e28f1b1101cd17d8310a96a9c726
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2021-05-04 15:55:13 +0200

    add setup for conv1d and swc

--- src/setup.py
+++ src/setup.py
@@ -7,4 +7,10 @@ setup(name='dcls_cpp',
 setup(name='dcls_full_cpp',
       ext_modules=[cpp_extension.CppExtension('dcls_full_cpp', ['src/dcls_full.cpp','src/cuda/dcls_full_cuda_kernel.cu'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
+setup(name='sparse_weight_conv_cpp',
+      ext_modules=[cpp_extension.CppExtension('sparse_weight_conv_cpp', ['src/sparse_weight_conv.cpp','src/cuda/sparse_weight_conv_cuda_kernel.cu'])],
+      cmdclass={'build_ext': cpp_extension.BuildExtension})
+setup(name='dcls_1d_cpp',
+      ext_modules=[cpp_extension.CppExtension('dcls_1d_cpp', ['src/dcls_1d.cpp','src/cuda/dcls_1d_cuda_kernel.cu'])],
+      cmdclass={'build_ext': cpp_extension.BuildExtension})
 
