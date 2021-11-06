e3b4807af9bd38a94e72b7211eda406de38e53ab
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2021-11-06 16:58:32 +0100

    dcls im2col: force single precision on grad

--- DCLS/functions/dcls_functionnal.py
+++ DCLS/functions/dcls_functionnal.py
@@ -28,12 +28,13 @@ class F_im2col_dcls2d(torch.autograd.Function):
 class dcls2d_conv(torch.autograd.Function):
 
     @staticmethod 
-    def forward(ctx, input, weight, P1, P2, bias, stride, padding, dilation, groups, gain):
+    def forward(ctx, input, weight, P1, P2, bias, stride, padding, dilation, groups, sign_grad, gain):
         
         ctx.stride = stride 
         ctx.padding = padding 
         ctx.dilation = dilation
         ctx.groups = groups
+        ctx.sign_grad = sign_grad        
         ctx.gain = gain
         
         ctx.save_for_backward(input, weight, P1, P2, bias)
@@ -54,14 +55,13 @@ class dcls2d_conv(torch.autograd.Function):
 
 
     @staticmethod   
-    def backward(ctx, grad_output):
-                  
+    def backward(ctx, grad_output):        
         input, weight, P1, P2, bias = ctx.saved_tensors
         outputs = dcls_2d.backward(input,
                                    weight, 
                                    P1, 
                                    P2, 
-                                   grad_output.contiguous(),
+                                   grad_output.float().contiguous(),# we force grad to be float32 for now
                                    bias,
                                    ctx.dilation[0], ctx.dilation[1],
                                    ctx.stride[0], ctx.stride[1],
@@ -69,6 +69,9 @@ class dcls2d_conv(torch.autograd.Function):
                                    ctx.groups,
                                    ctx.gain 
                                   )
+        if ctx.sign_grad :
+            grad_P1 = grad_P1.sign()
+            grad_P2 = grad_P2.sign()            
         
         grad_input, grad_weight, grad_P1, grad_P2, grad_bias = outputs
-        return grad_input, grad_weight, grad_P1, grad_P2, grad_bias, None, None, None, None, None   
+        return grad_input, grad_weight, grad_P1, grad_P2, grad_bias, None, None, None, None, None, None   
