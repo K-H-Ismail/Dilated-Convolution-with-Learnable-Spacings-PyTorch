500fe201e0443149866cfb86ce063b008b848b44
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2022-06-07 22:01:53 +0200

    dcls construct 1d, 3d: add support

--- DCLS/construct/functions/dcls_functionnal.py
+++ DCLS/construct/functions/dcls_functionnal.py
@@ -4,165 +4,169 @@ from torch.autograd.function import once_differentiable
 import torch.nn.functional as F
 import sys
 import os
-global eps 
+global eps
 eps = torch.finfo(torch.float32).eps
-import dcls_construct_1d, dcls_construct_2d, dcls_construct_3d, dcls_construct_2_1d, dcls_construct_3_1d, dcls_construct_3_2d
+import dcls_construct_1d, dcls_construct_2d, dcls_construct_3d#, dcls_construct_2_1d, dcls_construct_3_1d, dcls_construct_3_2d
 
 class ConstructKernel(torch.autograd.Function):
     pass
-    
+
 class ConstructKernel1d(ConstructKernel):
-    
-    @staticmethod 
-    def forward(ctx, weight, P, dilation, gain):
-        
-        ctx.dilation = dilation
-        ctx.gain = gain                 
-        
+
+    @staticmethod
+    def forward(ctx, weight, P, dilated_kernel_size, scaling):
+
+        ctx.dilated_kernel_size = dilated_kernel_size
+        ctx.scaling = scaling
+
         ctx.save_for_backward(weight, P)
-        
+
         output = dcls_construct_1d.forward(weight,
-                                       P,
-                                       ctx.dilation[0],
-                                       ctx.gain
-                                      )
+                                           P,
+                                           ctx.dilated_kernel_size[0],
+                                           ctx.scaling
+                                          )
 
         return output
 
-
-    @staticmethod  
+    @staticmethod
     def backward(ctx, grad_output):
-                  
+
         weight, P = ctx.saved_tensors
 
-        outputs = dcls_construct_1d.backward(weight, 
-                                         P, 
-                                         grad_output.contiguous(),
-                                         ctx.dilation[0],
-                                         ctx.gain
-                                        )
-        
+        outputs = dcls_construct_1d.backward(weight,
+                                             P,
+                                             grad_output.contiguous(),
+                                             ctx.dilated_kernel_size[0],
+                                             ctx.scaling
+                                            )
+
         grad_weight, grad_P = outputs
 
-
         return grad_weight, grad_P, None, None
-    
+
 class ConstructKernel2d(ConstructKernel):
-    
-    @staticmethod 
+
+    @staticmethod
     def forward(ctx, weight, P1, P2, dilated_kernel_size, scaling):
-        
-        ctx.dilated_kernel_size = dilated_kernel_size     
+
+        ctx.dilated_kernel_size = dilated_kernel_size
         ctx.scaling = scaling
 
-        
         ctx.save_for_backward(weight, P1, P2)
-        
+
         output = dcls_construct_2d.forward(weight,
-                                       P1, 
-                                       P2, 
-                                       ctx.dilated_kernel_size[0], ctx.dilated_kernel_size[1],
+                                           P1,
+                                           P2,
+                                           ctx.dilated_kernel_size[0],
+                                           ctx.dilated_kernel_size[1],
+                                           ctx.scaling
+                                          )
+        return output
+
+
+    @staticmethod
+    def backward(ctx, grad_output):
+
+        weight, P1, P2 = ctx.saved_tensors
+
+        outputs = dcls_construct_2d.backward(weight,
+                                             P1,
+                                             P2,
+                                             grad_output.contiguous(),
+                                             ctx.dilated_kernel_size[0],
+                                             ctx.dilated_kernel_size[1],
+                                             ctx.scaling
+                                            )
+
+        grad_weight, grad_P1, grad_P2 = outputs
+
+        return grad_weight, grad_P1, grad_P2, None, None
+
+class ConstructKernel3d(ConstructKernel):
+
+    @staticmethod
+    def forward(ctx, weight, P1, P2, P3, dilated_kernel_size, scaling):
+
+        ctx.dilated_kernel_size = dilated_kernel_size
+        ctx.scaling = scaling
+
+        ctx.save_for_backward(weight, P1, P2, P3)
+
+        output = dcls_construct_3d.forward(weight,
+                                       P1,
+                                       P2,
+                                       P3,
+                                       ctx.dilated_kernel_size[0],
+                                       ctx.dilated_kernel_size[1],
+                                       ctx.dilated_kernel_size[2],
                                        ctx.scaling
                                       )
 
         return output
 
 
-    @staticmethod   
+    @staticmethod
     def backward(ctx, grad_output):
-                  
-        weight, P1, P2 = ctx.saved_tensors
 
-        outputs = dcls_construct_2d.backward(weight, 
-                                         P1, 
-                                         P2, 
-                                         grad_output.contiguous(),
-                                         ctx.dilated_kernel_size[0], ctx.dilated_kernel_size[1],
-                                         ctx.scaling
-                                   )
-        
-        grad_weight, grad_P1, grad_P2 = outputs
-
-
-        return grad_weight, grad_P1, grad_P2, None, None
-
-class ConstructKernel3d(ConstructKernel):
-    
-    @staticmethod 
-    def forward(ctx, weight, P1, P2, P3, dilation):
-        
-        ctx.dilation = dilation        
-        
-        ctx.save_for_backward(weight, P1, P2, P3)
-        
-        output = dcls_construct_3d.forward(weight,
-                                       P1, 
-                                       P2,
-                                       P3,
-                                       ctx.dilation[0], ctx.dilation[1], ctx.dilation[2]
-                                      )
-
-        return output
-
-
-    @staticmethod   
-    def backward(ctx, grad_output):
-                  
         weight, P1, P2, P3 = ctx.saved_tensors
 
-        outputs = dcls_construct_3d.backward(weight, 
-                                         P1, 
+        outputs = dcls_construct_3d.backward(weight,
+                                         P1,
                                          P2,
                                          P3,
                                          grad_output.contiguous(),
-                                         ctx.dilation[0], ctx.dilation[1], ctx.dilation[2]
+                                         ctx.dilated_kernel_size[0],
+                                         ctx.dilated_kernel_size[1],
+                                         ctx.dilated_kernel_size[2],
+                                         ctx.scaling
                                    )
-        
+
         grad_weight, grad_P1, grad_P2, grad_P3 = outputs
 
 
-        return grad_weight, grad_P1, grad_P2, grad_P3, None
+        return grad_weight, grad_P1, grad_P2, grad_P3, None, None
 
 class ConstructKernel2_1d(ConstructKernel):
-    
-    @staticmethod 
+
+    @staticmethod
     def forward(ctx, weight, P, dilation):
-        
-        ctx.dilation = dilation        
-        
+
+        ctx.dilation = dilation
+
         ctx.save_for_backward(weight, P)
-        
+
         output = dcls_construct_2_1d.forward(weight,
                                        P,
                                        ctx.dilation[0]
                                       )
         return output
 
-    @staticmethod  
+    @staticmethod
     def backward(ctx, grad_output):
-                  
+
         weight, P = ctx.saved_tensors
 
-        outputs = dcls_construct_2_1d.backward(weight, 
-                                         P, 
+        outputs = dcls_construct_2_1d.backward(weight,
+                                         P,
                                          grad_output.contiguous(),
                                          ctx.dilation[0]
                                         )
-        
+
         grad_weight, grad_P = outputs
 
-        return grad_weight, grad_P, None 
-    
+        return grad_weight, grad_P, None
+
 class ConstructKernel3_1d(ConstructKernel):
-    
-    @staticmethod 
+
+    @staticmethod
     def forward(ctx, weight, P, dilation, gain):
-        
+
         ctx.dilation = dilation
-        ctx.gain        
-        
+        ctx.gain
+
         ctx.save_for_backward(weight, P)
-        
+
         output = dcls_construct_3_1d.forward(weight,
                                        P,
                                        ctx.dilation[0],
@@ -170,55 +174,55 @@ class ConstructKernel3_1d(ConstructKernel):
                                       )
         return output
 
-    @staticmethod  
+    @staticmethod
     def backward(ctx, grad_output):
-                  
+
         weight, P = ctx.saved_tensors
 
-        outputs = dcls_construct_3_1d.backward(weight, 
-                                         P, 
+        outputs = dcls_construct_3_1d.backward(weight,
+                                         P,
                                          grad_output.contiguous(),
                                          ctx.dilation[0],
                                          ctx.gain
                                         )
-        
+
         grad_weight, grad_P = outputs
 
         return grad_weight, grad_P, None
 
 class ConstructKernel3_2d(ConstructKernel):
-    
-    @staticmethod 
+
+    @staticmethod
     def forward(ctx, weight, P1, P2, dilation):
-        
-        ctx.dilation = dilation        
-        
+
+        ctx.dilation = dilation
+
         ctx.save_for_backward(weight, P1, P2)
-        
+
         output = dcls_construct_3_2d.forward(weight,
-                                       P1, 
-                                       P2, 
+                                       P1,
+                                       P2,
                                        ctx.dilation[0], ctx.dilation[1]
                                       )
 
         return output
 
 
-    @staticmethod   
+    @staticmethod
     def backward(ctx, grad_output):
-                  
+
         weight, P1, P2 = ctx.saved_tensors
 
-        outputs = dcls_construct_3_2d.backward(weight, 
-                                         P1, 
-                                         P2, 
+        outputs = dcls_construct_3_2d.backward(weight,
+                                         P1,
+                                         P2,
                                          grad_output.contiguous(),
                                          ctx.dilation[0], ctx.dilation[1]
                                    )
-        
+
         grad_weight, grad_P1, grad_P2 = outputs
 
 
         return grad_weight, grad_P1, grad_P2, None
-    
+
 
