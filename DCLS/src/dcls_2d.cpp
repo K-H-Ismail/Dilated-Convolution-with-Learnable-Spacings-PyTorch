538559765f01400158faf8ab4157c34faf77ad4d
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2022-07-04 10:55:19 +0200

    dcls im2col change signature

--- DCLS/src/dcls_2d.cpp
+++ DCLS/src/dcls_2d.cpp
@@ -7,30 +7,30 @@
 // CUDA forward declarations
 
 torch::Tensor dcls_2d_cuda_forward(
-    torch::Tensor input,    
+    torch::Tensor input,
     torch::Tensor weight,
     torch::Tensor P1,
     torch::Tensor P2,
     c10::optional<torch::Tensor> bias,
-    const int dilation_h, const int dilation_w, 
-    const int stride_h, const int stride_w, 
-    const int padding_h, const int padding_w, 
+    const int dilated_kernel_size_h, const int dilated_kernel_size_w,
+    const int stride_h, const int stride_w,
+    const int padding_h, const int padding_w,
     const int groups,
-    const float gain); 
+    const float scaling);
 
 std::vector<torch::Tensor> dcls_2d_cuda_backward(
-    torch::Tensor input,    
+    torch::Tensor input,
     torch::Tensor weight,
     torch::Tensor P1,
     torch::Tensor P2,
-    torch::Tensor grad_output,      
+    torch::Tensor grad_output,
     c10::optional<torch::Tensor> bias,
-    const int dilation_h, const int dilation_w, 
-    const int stride_h, const int stride_w, 
-    const int padding_h, const int padding_w, 
+    const int dilated_kernel_size_h, const int dilated_kernel_size_w,
+    const int stride_h, const int stride_w,
+    const int padding_h, const int padding_w,
     const int groups,
-    const float gain); 
-    
+    const float scaling);
+
 // C++ interface
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
@@ -38,67 +38,67 @@ std::vector<torch::Tensor> dcls_2d_cuda_backward(
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 torch::Tensor dcls_2d_forward(
-    torch::Tensor input,    
+    torch::Tensor input,
     torch::Tensor weight,
     torch::Tensor P1,
     torch::Tensor P2,
     c10::optional<torch::Tensor> bias,
-    const int dilation_h, const int dilation_w, 
-    const int stride_h, const int stride_w, 
-    const int padding_h, const int padding_w, 
+    const int dilated_kernel_size_h, const int dilated_kernel_size_w,
+    const int stride_h, const int stride_w,
+    const int padding_h, const int padding_w,
     const int groups,
-    const float gain) {
+    const float scaling) {
 
-    //CHECK_INPUT(input);    
+    //CHECK_INPUT(input);
     CHECK_INPUT(weight);
     CHECK_INPUT(P1);
     CHECK_INPUT(P2);
     //CHECK_INPUT(bias);
 
 
-    return dcls_2d_cuda_forward(input, 
-                              weight, 
-                              P1, 
+    return dcls_2d_cuda_forward(input,
+                              weight,
+                              P1,
                               P2,
                               bias,
-                              dilation_h, dilation_w,
+                              dilated_kernel_size_h, dilated_kernel_size_w,
                               stride_h, stride_w,
                               padding_h, padding_w,
                               groups,
-                              gain); 
+                              scaling);
 }
 
 std::vector<torch::Tensor> dcls_2d_backward(
-    torch::Tensor input,    
+    torch::Tensor input,
     torch::Tensor weight,
     torch::Tensor P1,
     torch::Tensor P2,
-    torch::Tensor grad_output,      
+    torch::Tensor grad_output,
     c10::optional<torch::Tensor> bias,
-    const int dilation_h, const int dilation_w, 
-    const int stride_h, const int stride_w, 
-    const int padding_h, const int padding_w, 
+    const int dilated_kernel_size_h, const int dilated_kernel_size_w,
+    const int stride_h, const int stride_w,
+    const int padding_h, const int padding_w,
     const int groups,
-    const float gain) {
-    
-    //CHECK_INPUT(input);    
+    const float scaling) {
+
+    //CHECK_INPUT(input);
     CHECK_INPUT(weight);
     CHECK_INPUT(P1);
     CHECK_INPUT(P2);
-    CHECK_INPUT(grad_output);    
+    CHECK_INPUT(grad_output);
     //CHECK_INPUT(bias);
 
-    return dcls_2d_cuda_backward(input, 
-                              weight, 
-                              P1, 
+    return dcls_2d_cuda_backward(input,
+                              weight,
+                              P1,
                               P2,
                               grad_output,
                               bias,
-                              dilation_h, dilation_w,
+                              dilated_kernel_size_h, dilated_kernel_size_w,
                               stride_h, stride_w,
                               padding_h, padding_w,
                               groups,
-                              gain); 
+                              scaling);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
--- DCLS/src/im2col_dcls_2d.cpp
+++ DCLS/src/im2col_dcls_2d.cpp
@@ -2,11 +2,11 @@
 #include <torch/extension.h>
 
 torch::Tensor im2col_dcls_2d_batch_cuda(torch::Tensor im,
-                               torch::Tensor P_h, torch::Tensor P_w,                      
-                               const int dilation_h, const int dilation_w,
+                               torch::Tensor P_h, torch::Tensor P_w,
+                               const int dilated_kernel_size_h, const int dilated_kernel_size_w,
                                const int padding_h, const int padding_w,
                                const int stride_h, const int stride_w,
-                               const int height_out, const int width_out                            
+                               const int height_out, const int width_out
                                );
 // C++ interface
 
@@ -15,25 +15,25 @@ torch::Tensor im2col_dcls_2d_batch_cuda(torch::Tensor im,
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 torch::Tensor im2col_dcls_2d_forward(torch::Tensor im,
-                          torch::Tensor P_h, torch::Tensor P_w,                      
-                          const int dilation_h, const int dilation_w,
+                          torch::Tensor P_h, torch::Tensor P_w,
+                          const int dilated_kernel_size_h, const int dilated_kernel_size_w,
                           const int padding_h, const int padding_w,
                           const int stride_h, const int stride_w,
-                          const int height_out, const int width_out,                                  
-                          const int shift_h, const int shift_w                                    
+                          const int height_out, const int width_out,
+                          const int shift_h, const int shift_w
                           )
-{   
+{
     CHECK_INPUT(im);
     CHECK_INPUT(P_h);
-    CHECK_INPUT(P_w);    
+    CHECK_INPUT(P_w);
 
     return im2col_dcls_2d_batch_cuda(im,
-                            P_h, P_w,    
-                            dilation_h, dilation_w,
-                            padding_h, padding_w,                              
+                            P_h, P_w,
+                            dilated_kernel_size_h, dilated_kernel_size_w,
+                            padding_h, padding_w,
                             stride_h, stride_w,
                             height_out, width_out
-                            ); 
+                            );
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
